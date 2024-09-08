a2d7c197679c0fc1baf13cd811d977a5ff060f46
John Rhay Domingo <jhnrhyyyyyyyyyy@gmail.com>
2024-09-08 22:09:45 +0800

    Act#3 Numbers

---
+++ number.js
@@ -0,0 +1,22 @@
+let firstFavNum = 5;
+let secondFavNum = 17; 
+console.log("My first favorite number is", firstFavNum);
+console.log("My second favorite number is", secondFavNum);
+
+let sum = firstFavNum + secondFavNum;
+console.log("\n\nAddition:", sum);
+
+let difference = firstFavNum - secondFavNum;
+console.log("Subtraction:", difference);
+
+let product = firstFavNum * secondFavNum;
+console.log("Multiplication:", product);
+
+let quotient = firstFavNum / secondFavNum;
+console.log("Division:", quotient);
+
+let modulus = firstFavNum % secondFavNum;
+console.log("Modulus:", modulus);
+
+let exponentiation = firstFavNum ** secondFavNum;
+console.log("Exponentiation:", exponentiation);
